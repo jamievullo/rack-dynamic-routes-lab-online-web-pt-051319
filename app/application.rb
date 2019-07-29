9f20bd5f54498059691fe263ba2a24aad5c75dd3
Jamie Vullo <jvullo@protonmail.com>
2019-07-29 17:58:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,20 +26,3 @@ class Application
     resp.finish
   end
 end 
-#     if req.path.match(/items/)
-#       item_name = req.path.split("/items/").last
-
-#       item = @@items.find{|i| i.name == item_name}
-
-#     if item.nil?
-#       resp.write "Item not found"
-#       resp.status = 400
-#     else
-#       resp.write item.price
-#     end
-#       resp.write "Route not found"
-#       resp.status = 404
-#     end
-#     resp.finish
-#   end
-# end
